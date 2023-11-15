a171b6c4a28f5b5942db486ea97f141cc1170901
edo1z <endo.yuta@logicky.com>
2023-11-16 00:01:22 +0900

    DBに接続したで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use sea_orm::Database;
+use sea_orm::DatabaseConnection;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), sea_orm::DbErr> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URLを.envで設定してないで！");
+    let db: DatabaseConnection = Database::connect(&database_url).await?;
+    println!("データベースに接続できたで！");
+    Ok(())
 }
